52ebdf7693574a9b108e8e39bf343cebe7e75135
Your Name <you@example.com>
2025-04-28 15:18:17 +0300

    любое вложение

--- collect_files.sh
+++ collect_files.sh
@@ -11,6 +11,6 @@ OUTPUT_DIR="$2"
 echo "INPUT_DIR=\"$INPUT_DIR\""
 echo "OUTPUT_DIR=\"$OUTPUT_DIR\""
 
-find "$INPUT_DIR" -maxdepth 2 -type f | while IFS= read -r file; do
+find "$INPUT_DIR" -type f | while IFS= read -r file; do
   cp "$file" "$OUTPUT_DIR/$(basename "$file")"
 done
