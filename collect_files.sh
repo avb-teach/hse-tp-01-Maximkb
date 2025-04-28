d669884b3d47cb25bda1d1113063b84b7c02ef7a
Your Name <you@example.com>
2025-04-28 15:59:18 +0300

    допустимая глубина 2

--- collect_files.sh
+++ collect_files.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 MAX_DEPTH INPUT_DIR OUTPUT_DIR"
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+  echo "Usage: $0 INPUT_DIR OUTPUT_DIR [MAX_DEPTH]"
   exit 1
 fi
 
-MAX_DEPTH="$1"
-INPUT_DIR="$2"
-OUTPUT_DIR="$3"
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+MAX_DEPTH=""
 
-echo "MAX_DEPTH=\"$MAX_DEPTH\""
+if [ $# -eq 3 ]; then
+  MAX_DEPTH="$3"
+fi
+
 echo "INPUT_DIR=\"$INPUT_DIR\""
 echo "OUTPUT_DIR=\"$OUTPUT_DIR\""
+echo "MAX_DEPTH=\"$MAX_DEPTH\""
 
-find "$INPUT_DIR" -maxdepth "$MAX_DEPTH" -type f | while IFS= read -r file; do
-  cp "$file" "$OUTPUT_DIR/$(basename "$file")"
-done
+if [ -n "$MAX_DEPTH" ]; then
+  find "$INPUT_DIR" -maxdepth "$MAX_DEPTH" -type f | \
+    while IFS= read -r file; do
+      cp "$file" "$OUTPUT_DIR/$(basename "$file")"
+    done
+else
+  find "$INPUT_DIR" -type f | \
+    while IFS= read -r file; do
+      cp "$file" "$OUTPUT_DIR/$(basename "$file")"
+    done
+fi
