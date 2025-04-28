07c8967193818ecd1deb3e40b91e1445f246bf5e
Your Name <you@example.com>
2025-04-28 15:00:23 +0300

    2 параметра

--- collect_files.sh
+++ collect_files.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 INPUT_DIR OUTPUT_DIR"
+  exit 1
+fi
+
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+
+echo "INPUT_DIR=\"$INPUT_DIR\""
+echo "OUTPUT_DIR=\"$OUTPUT_DIR\""
